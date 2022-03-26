9d9bd3c76ee1bef1eff107c7ab824c6bcbefc2cb
Ann <cchouaa@connect.ust.hk>
2022-03-26 16:04:49 +0800

    edit post mutation changed

--- src/utils/graphql/mutations.js
+++ src/utils/graphql/mutations.js
@@ -64,7 +64,7 @@ export const CREATE_POST = gql`
 export const EDIT_POST = gql`
     mutation editPost(
         $postId: ID!
-        $postInput: PostInput
+        $postInput: EditPostInput
     ) {
         editPost(
             postId: $postId,
